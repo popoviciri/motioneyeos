88d52adff6980819bdd6e20c0588f2699794db5d
popoviciri <radoo.popovici@gmail.com>
2020-01-30 07:30:10 +0100

    motion 4.3.0

--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOTION_VERSION = 10807b1d964424b708d0d5ce7027a6669f80355a
+MOTION_VERSION = release-4.3.0
 MOTION_SITE = $(call github,Motion-Project,motion,$(MOTION_VERSION))
 MOTION_AUTORECONF = YES
 MOTION_DEPENDENCIES = host-pkgconf ffmpeg jpeg libmicrohttpd
