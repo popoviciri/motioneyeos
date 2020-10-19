b83ef4e2cfe08412991a14a8b996bb9a72531cd5
Radu Popovici <radu@server.felo>
2020-10-19 16:21:52 +0200

    that wasn't right

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 154de7bbd5844a824a635d4f9e3f773c15c6ce11
+RPI_FIRMWARE_VERSION = 358f4c271f2bc10931aae8fe0879ffaded8b9c44
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
